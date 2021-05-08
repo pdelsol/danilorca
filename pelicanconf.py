bca5b6d44f4d3428bf162c544fc858367faba92a
Patricio del Sol <pato@admetricks.com>
2021-05-08 18:18:39 -0400

    fe

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,6 @@ LINKS = (("Babytuto", "https://www.babytuto.com/"),
 SOCIAL = (("LinkedIn", "https://www.linkedin.com/in/danielalorcanunez/"),
 		 )
 
-
 DEFAULT_PAGINATION = 100
 
 # Uncomment following line if you want document-relative URLs when developing
