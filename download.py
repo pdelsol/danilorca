bd87157809f935cd8389f748fe6aed6cc5b7c187
Patricio del Sol <pdelsol@gmail.com>
2022-06-29 21:49:32 -0400

    ✨ feat: get mp3 from episode url

--- download.py
+++ download.py
@@ -74,18 +74,20 @@ def download_data(day, podcast):
         json.dump(podcast, outfile, indent=2, sort_keys=True)
 
 
-URL = "https://www.radioagricultura.cl/podcast_programas/en-prendete/"
-page = requests.get(URL)
+page = requests.get("https://www.radioagricultura.cl/podcast_programas/en-prendete/")
 soup = BeautifulSoup(page.content, "html.parser")
 for episode in soup.find_all("article", {"class": "podcast"}):
     title = episode.find("h2").find("a").get_text()
     day = episode.find("time")["datetime"][:10]
-    if episode.find("audio"):
-        if episode.find("audio") and episode.find("audio").has_attr("src"):
-            mp3 = episode.find("audio")["src"]
-        elif episode.find("audio").find("source").has_attr("src"):
-            mp3 = episode.find("audio").find("source")["src"]
+    print(f"{day}: {title}")
+    episode_page = requests.get(episode.find("h2").find("a")["href"])
+    episode_soup = BeautifulSoup(episode_page.content, "html.parser")
+    if episode_soup.find("audio"):
+        if episode_soup.find("audio") is not None and episode_soup.find("audio").has_attr("src"):
+            mp3_link = episode_soup.find("audio")["src"]
+        elif episode_soup.find("audio").find("source").has_attr("src"):
+            mp3_link = episode_soup.find("audio").find("source")["src"]
         else:
             print(f"{day}: ERROR: COULDNT FIND AUDIO FILE")
             continue
-    download_podcast(day, title, mp3)
+        download_podcast(day, title, mp3_link)
